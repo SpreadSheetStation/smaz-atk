e09c4b2fc365127350ff62326b3ecfa2c1b4db9f
fuciusteez <103060249+fuciusteez@users.noreply.github.com>
2026-08-18 13:35:19 +0000

    upd

---
+++ atk.py
@@ -0,0 +1,17 @@
+
+
+
+
+## For new env set-ups in CodeSpaces
+
+# Create .venv (standard name, but you can call it anything)
+python -m venv .venv
+
+# Activate it
+source .venv/bin/activate
+
+# Install Jupyter inside it
+pip install jupyter
+
+# Run notebook
+jupyter notebook
